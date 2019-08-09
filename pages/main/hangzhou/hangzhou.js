76df73dba254f0b7abe76eef34191e4aec763610
Diao Zhongpu <dzp@zju.edu.cn>
2019-08-10 06:37:36 +0800

    Initial commit

---
+++ pages/main/hangzhou/hangzhou.js
@@ -0,0 +1,43 @@
+Page({
+  onShow() {
+    wx.reportAnalytics('enter_home_programmatically', {})
+  },
+  onShareAppMessage() {
+    return {
+      title: '小程序官方组件展示',
+      path: 'page/component/index'
+    }
+  },
+
+  data: {
+    list: [
+      {
+        id: 'xihu',
+        name: '西湖',
+        open: false,
+        pages: ['简介','情况','评论']
+      }, {
+        id: 'lingyinsi',
+        name: '灵隐寺',
+        open: false,
+        pages: ['简介', '情况', '评论']
+      }
+    ]
+  },
+
+  kindToggle(e) {
+    const id = e.currentTarget.id
+    const list = this.data.list
+    for (let i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id === id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list
+    })
+    wx.reportAnalytics('click_view_programmatically', {})
+  }
+})
