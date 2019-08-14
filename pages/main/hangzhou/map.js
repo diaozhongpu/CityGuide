daa60a66e6f15f804d09cb66b157e72d92888fb7
Diao Zhongpu <dzp@zju.edu.cn>
2019-08-14 16:36:04 +0800

    stretch

---
+++ pages/main/hangzhou/map.js
@@ -0,0 +1,32 @@
+Page({
+  data: {
+    markers: [{
+      iconPath: "/pages/picture/zju.jpg",
+      id: 0,
+      latitude: 30.30200,
+      longitude: 120.07965,
+      width: 50,
+      height: 50
+    }],
+    controls: [{
+      id: 1,
+      iconPath: '/pages/picture/logo.jpg',
+      position: {
+        left: 0,
+        top: 50 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
+  },
+  regionchange(e) {
+    console.log(e.type)
+  },
+  markertap(e) {
+    console.log(e.markerId)
+  },
+  controltap(e) {
+    console.log(e.controlId)
+  }
+})
